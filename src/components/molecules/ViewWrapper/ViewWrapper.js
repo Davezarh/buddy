dc9e8daa1daa891739a87dd6c43a9ca936c9f55c
Dawid Harendarz <68152541+Davezarh@users.noreply.github.com>
2021-04-08 16:07:38 +0200

    Added global style

---
+++ src/components/molecules/ViewWrapper/ViewWrapper.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+export const ViewWrapper = styled.div`
+  background-color: ${({ theme }) => theme.colors.white};
+  margin: 25px;
+  width: 100%;
+  max-width: 500px;
+  padding: 40px 30px;
+  border-radius: 25px;
+  box-shadow: 0 5px 15px - 10px rgba(0, 0, 0, 0.3);
+`;
